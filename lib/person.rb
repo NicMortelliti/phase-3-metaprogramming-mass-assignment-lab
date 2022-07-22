259c07d115aff7ee116b4e510f95039d1a50e5e9
NicMortelliti <npmortelliti@gmail.com>
2022-07-22 14:27:45 -0700

    commit tree caa1461eebf89f15f523bb003573c0ee41ccca2e

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
-  # your code here
+  def initialize(args)
+    args.each do |key, value|
+      # create a getter and setter by calling the attr_accessor method
+      self.class.attr_accessor(key)
+      
+      # Create attribute
+      self.send("#{key}=", value)
+    end
+  end
 end
